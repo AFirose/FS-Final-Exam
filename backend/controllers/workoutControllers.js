4ad1dcb14eda0e2d8ad87de5a71fbd109e137b12
AFirose <afrin.techpm@gmail.com>
2026-03-12 14:18:30 +0200

    Q3: Implement updateWorkout

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -41,14 +41,32 @@ const { workoutId } = req.perams.workoutId;
 // - Return the updated workout as JSON
 // - Return 404 with { error: "Workout not found" } if not found
 const updateWorkout = async (req, res) => {
-  res.send("updateWorkout - not yet implemented");
+  const { workoutId } = req.params.workoutId;
+
+  if (!mongoose.Types.ObjectId.isValid(workoutId)) {
+    return res.status(400).json({ message: "Invalid workout ID" });
+  }
+
+  try {
+    const updatedWorkout = await Workout.findOneAndUpdate(
+      { _id: workoutId },
+      { ...req.body },
+      { returnDocument: "after" }
+    );
+    if (updatedWorkout) {
+      res.status(200).json(updatedWorkout);
+    } else {
+      res.status(404).json({ message: "Workout not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update Workout" });
+  }
 };
 
 // TODO (Q2): Implement deleteWorkout
 // - Delete the workout by req.params.workoutId
 // - Return the deleted workout as JSON
 // - Return 404 with { error: "Workout not found" } if not found
-
 const deleteWorkout = async (req, res) => {
   const { workoutId } = req.params.workoutId;
 
